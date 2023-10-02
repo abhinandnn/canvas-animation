b3a7d54ebac0362a4dddc764fa8969dceeb68a74
Abhinandan Pandey <abhinandan2210049@akgec.ac.in>
2023-10-03 04:13:38 +0530

    added intro and background

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const canvas = document.getElementById("canva");
 const ctx = canvas.getContext("2d");
-
 const r = 30;
 const speed = 2;
-const colors = ["red","green","blue", "orange","purple","yellow","pink" ,"white"];
+const colors = ["red","green","blue", "orange","purple","yellow","pink" ,"white","grey"];
 let balloons = [];
 let score = 0;
 let over = false;
@@ -12,13 +11,16 @@ function printscore() {
     ctx.fillStyle = "black";
     ctx.fillText("Score: " + score, 20, 40);
 }
+function clear() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
 function newBalloon() {
-    const balloonColor = colors[Math.floor(Math.random() * colors.length)];
+    const bcolor = colors[Math.floor(Math.random() * colors.length)];
     const balloon = {
         x: Math.random() * (canvas.width - 2 * r) + r,
         y: canvas.height,
-        color: balloonColor
+        color: bcolor
     };
     balloons.push(balloon);
 }
@@ -33,8 +35,7 @@ function updateGameArea() {
         return;
     }
     else{
-    clearCanvas();
-
+    clear();
     for (const balloon of balloons) {
         ctx.beginPath();
         ctx.arc(balloon.x, balloon.y, r, 0, Math.PI * 2);
@@ -53,31 +54,28 @@ function updateGameArea() {
         ctx.fillStyle = "black";
         ctx.fillText("Score: " + score, 20, 40);
     }
-canvas.addEventListener("click", popBalloon);
-function popBalloon(event) {
+    printscore();
+    setTimeout(updateGameArea, 16);}
+}
+canvas.addEventListener("click", pop);
+function pop(event) {
     if (over) return;
 
-    const mouseX = event.clientX - canvas.offsetLeft;
-    const mouseY = event.clientY - canvas.offsetTop;
+    const mx = event.clientX - canvas.offsetLeft;
+    const my = event.clientY - canvas.offsetTop;
 
     for (let i = balloons.length - 1; i >= 0; i--) {
         const balloon = balloons[i];
-        const distance = Math.sqrt((mouseX - balloon.x) ** 2 + (mouseY - balloon.y) ** 2);
+        const distance = Math.sqrt((mx - balloon.x)**2 + (my - balloon.y)**2);
 
         if (distance <= r) {
             balloons.splice(i, 1);
+            // if(balloons[i].color=="white")
+            // {score++;}
             score++;
             return;
         }
     }
 }
-
-    printscore();
-    setTimeout(updateGameArea, 16);}
-}
-
-function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
 updateGameArea();
-setInterval(newBalloon, 2000);
+setInterval(newBalloon, 1500);
